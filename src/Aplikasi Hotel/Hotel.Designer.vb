acc841e8ba8841a378c3aa98c1de91af3759021e
Fiqri317 <139039571+Fiqri317@users.noreply.github.com>
2024-10-18 18:09:23 +0700

    main

---
+++ src/Aplikasi Hotel/Hotel.Designer.vb
@@ -0,0 +1,329 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Hotel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.dgv_coba = New System.Windows.Forms.DataGridView()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Bs_coba = New System.Windows.Forms.BindingSource(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.dgv_coba, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Bs_coba, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker2)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Black
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(247, 171)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Registrasi"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(106, 140)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(133, 20)
+        Me.TextBox3.TabIndex = 9
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.CustomFormat = "dd MMMM yyyy"
+        Me.DateTimePicker2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker2.Location = New System.Drawing.Point(106, 108)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(133, 20)
+        Me.DateTimePicker2.TabIndex = 8
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "dd MMMM yyyy"
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(106, 77)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(133, 20)
+        Me.DateTimePicker1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(106, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(133, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(106, 20)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(133, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(7, 143)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Total"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(7, 114)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Tanggal Checkout"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(7, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Tanggal Checkin"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "No Identitas"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 199)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(177, 83)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Aksi"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(91, 48)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Reset"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(91, 19)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Button2.Location = New System.Drawing.Point(10, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Hapus"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(10, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Tambah"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button5)
+        Me.GroupBox3.Controls.Add(Me.TextBox4)
+        Me.GroupBox3.Controls.Add(Me.ComboBox1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(13, 300)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(205, 80)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Pencarian"
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(137, 20)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(57, 23)
+        Me.Button5.TabIndex = 2
+        Me.Button5.Text = "Cari"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(10, 49)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(184, 20)
+        Me.TextBox4.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"No Identitas", "Nama", "Tanggal Checkin", "Tanggal Checkout", "Total"})
+        Me.ComboBox1.Location = New System.Drawing.Point(10, 22)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "Pilih"
+        '
+        'dgv_coba
+        '
+        Me.dgv_coba.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_coba.Location = New System.Drawing.Point(277, 13)
+        Me.dgv_coba.Name = "dgv_coba"
+        Me.dgv_coba.Size = New System.Drawing.Size(360, 198)
+        Me.dgv_coba.TabIndex = 3
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(277, 232)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(84, 23)
+        Me.Button6.TabIndex = 4
+        Me.Button6.Text = "Keluar"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Hotel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(649, 392)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.dgv_coba)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Hotel"
+        Me.Text = "Hotel"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.dgv_coba, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Bs_coba, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents dgv_coba As System.Windows.Forms.DataGridView
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Bs_coba As System.Windows.Forms.BindingSource
+End Class
---
+++ src/Aplikasi Hotel/Hotel.vb
@@ -0,0 +1,204 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.OleDb
+Public Class Hotel
+    Dim _koneksiString As String
+    Dim _koneksi As New OleDbConnection
+    Dim komandambil As New OleDbCommand
+    Dim datatabelku As New DataTable
+    Dim dataadapterku As New OleDbDataAdapter
+    Dim x As String
+
+    Private Sub dgv_coba_CellFormatting(ByVal sender As Object, ByVal e As DataGridViewCellFormattingEventArgs) Handles dgv_coba.CellFormatting
+        If dgv_coba.Columns(e.ColumnIndex).Name = "Tanggal Checkin" Then
+            If e.Value IsNot Nothing Then
+                Dim tgl As DateTime = Convert.ToDateTime(e.Value)
+                e.Value = tgl.ToString("dd MMMM yyyy", New System.Globalization.CultureInfo("id-ID"))
+                e.FormattingApplied = True
+            End If
+        End If
+
+        If dgv_coba.Columns(e.ColumnIndex).Name = "Tanggal Checkout" Then
+            If e.Value IsNot Nothing Then
+                Dim tgl As DateTime = Convert.ToDateTime(e.Value)
+                e.Value = tgl.ToString("dd MMMM yyyy", New System.Globalization.CultureInfo("id-ID"))
+                e.FormattingApplied = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Hotel_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        _koneksiString = "Provider=Microsoft.Jet.OleDb.4.0;" + "Data Source=D:\Campus\Semester V\Kecerdasan Komputasi\Aplikasi Hotel\database\Hotel.mdb;"
+        _koneksi.ConnectionString = _koneksiString
+        _koneksi.Open()
+
+        komandambil.Connection = _koneksi
+        komandambil.CommandType = CommandType.Text
+
+        komandambil.CommandText = "SELECT * FROM Hotel"
+        dataadapterku.SelectCommand = komandambil
+        dataadapterku.Fill(datatabelku)
+        Bs_coba.DataSource = datatabelku
+        dgv_coba.DataSource = Bs_coba
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.ValueChanged
+        HitungTotalHari()
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker2.ValueChanged
+        HitungTotalHari()
+    End Sub
+
+    Private Sub HitungTotalHari()
+        Dim tglCheckIn As DateTime = DateTimePicker1.Value
+        Dim tglCheckOut As DateTime = DateTimePicker2.Value
+        Dim totalHari As Integer = (tglCheckOut - tglCheckIn).Days
+
+        If totalHari >= 0 Then
+            TextBox3.Text = totalHari.ToString() + " Hari"
+        Else
+            MessageBox.Show("Tanggal Checkout tidak boleh sebelum Tanggal Checkin!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            TextBox3.Clear()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim cmdTambah As New OleDbCommand
+        Dim tanya As String
+        Dim x As DataRow
+        cmdTambah.Connection = _koneksi
+        cmdTambah.CommandText = "INSERT INTO " + "Hotel ([No Identitas], Nama, [Tanggal Checkin], [Tanggal Checkout], Total)" +
+            "VALUES ('" + TextBox1.Text + "','" + TextBox2.Text + "','" + DateTimePicker1.Text + "','" +
+            DateTimePicker2.Text + "','" + TextBox3.Text + " ')"
+        tanya = MessageBox.Show("Data Ini di Simpan ?", "info", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If tanya = vbYes Then
+            cmdTambah.ExecuteNonQuery()
+            x = datatabelku.NewRow
+            x("No Identitas") = TextBox1.Text
+            x("Nama") = TextBox2.Text
+            x("Tanggal Checkin") = DateTimePicker1.Text
+            x("Tanggal Checkout") = DateTimePicker2.Text
+            x("Total") = TextBox3.Text
+            datatabelku.Rows.Add(x)
+            Bs_coba.DataSource = Nothing
+            Bs_coba.DataSource = datatabelku
+
+            dgv_coba.Refresh()
+            Bs_coba.MoveFirst()
+        End If
+    End Sub
+
+    Private Sub dgv_coba_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgv_coba.CellContentClick
+        TextBox1.Text = dgv_coba.CurrentRow.Cells(0).Value.ToString()
+        TextBox2.Text = dgv_coba.CurrentRow.Cells(1).Value.ToString()
+        DateTimePicker1.Value = Convert.ToDateTime(dgv_coba.CurrentRow.Cells(2).Value)
+        DateTimePicker2.Value = Convert.ToDateTime(dgv_coba.CurrentRow.Cells(3).Value)
+        TextBox3.Text = dgv_coba.CurrentRow.Cells(4).Value.ToString()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim cmdHapus As New OleDbCommand
+        cmdHapus.Connection = _koneksi
+        cmdHapus.CommandType = CommandType.Text
+        x = MessageBox.Show("Yakin Data Akan di Hapus ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If x = vbYes Then
+            cmdHapus.CommandText = "DELETE FROM " + "Hotel WHERE [No Identitas]=" + TextBox1.Text
+            cmdHapus.ExecuteNonQuery()
+        End If
+        Bs_coba.RemoveCurrent()
+        dgv_coba.Refresh()
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+        DateTimePicker1.Value = DateTime.Now
+        DateTimePicker2.Value = DateTime.Now
+        TextBox3.Clear()
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim cmdUpdate As New OleDbCommand
+        cmdUpdate.Connection = _koneksi
+        cmdUpdate.CommandType = CommandType.Text
+        x = MessageBox.Show("Yakin Data Ingin di Perbarui?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If x = vbYes Then
+            cmdUpdate.CommandText = "UPDATE Hotel SET " +
+                "Nama = '" + TextBox2.Text + "', " +
+                "[Tanggal Checkin] = '" + DateTimePicker1.Text + "', " +
+                "[Tanggal Checkout] = '" + DateTimePicker2.Text + "', " +
+                "Total = '" + TextBox3.Text + "' " +
+                "WHERE [No Identitas] = " + TextBox1.Text  '
+            cmdUpdate.ExecuteNonQuery()
+            Dim rowToUpdate As DataRow = datatabelku.Select("[No Identitas] = " + TextBox1.Text).FirstOrDefault()
+            If rowToUpdate IsNot Nothing Then
+                rowToUpdate("No Identitas") = TextBox1.Text
+                rowToUpdate("Nama") = TextBox2.Text
+                rowToUpdate("Tanggal Checkin") = DateTimePicker1.Text
+                rowToUpdate("Tanggal Checkout") = DateTimePicker2.Text
+                rowToUpdate("Total") = TextBox3.Text
+            End If
+            Bs_coba.DataSource = Nothing
+            Bs_coba.DataSource = datatabelku
+            dgv_coba.Refresh()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        DateTimePicker1.Value = DateTime.Now
+        DateTimePicker2.Value = DateTime.Now
+        TextBox3.Clear()
+        TextBox4.Clear()
+        ComboBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        datatabelku.Clear()
+        Dim kolomPencarian As String = ""
+        If ComboBox1.SelectedItem Is Nothing Then
+            MessageBox.Show("Pilih kolom pencarian terlebih dahulu!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Select Case ComboBox1.SelectedItem.ToString()
+            Case "No Identitas"
+                kolomPencarian = "[No Identitas]"
+            Case "Nama"
+                kolomPencarian = "Nama"
+            Case "Tanggal Checkin"
+                kolomPencarian = "[Tanggal Checkin]"
+            Case "Tanggal Checkout"
+                kolomPencarian = "[Tanggal Checkout]"
+            Case "Total"
+                kolomPencarian = "Total"
+            Case Else
+                MessageBox.Show("Kolom pencarian tidak valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+        End Select
+
+        ' Pastikan ada inputan pencarian
+        If TextBox4.Text = "" Then
+            MessageBox.Show("Masukkan data pencarian!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        komandambil.Connection = _koneksi
+        komandambil.CommandType = CommandType.Text
+        komandambil.CommandText = "SELECT * FROM Hotel WHERE " + kolomPencarian + " LIKE '%" + TextBox4.Text + "%'"
+        dataadapterku.SelectCommand = komandambil
+        dataadapterku.Fill(datatabelku)
+        dgv_coba.Refresh()
+        Bs_coba.DataSource = datatabelku
+        dgv_coba.DataSource = Bs_coba
+        Bs_coba.MoveFirst()
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Me.Close()
+        Login.Show()
+        Login.TextBox1.Clear()
+        Login.TextBox2.Clear()
+    End Sub
+End Class
